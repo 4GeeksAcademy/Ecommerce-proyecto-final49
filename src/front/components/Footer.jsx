6e968e0cb76442b73d9184282eb5e06511161498
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-19 17:19:54 +0000

    el footer fue modificado

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,54 @@
+import { Link } from "react-router-dom";
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+	<footer className="fixed-bottom py-3 bg-dark text-white">
+		<div className="container">
+			<div className="row">
+				<div className="col-12 col-md-4 ">
+					<h4>Catergorias</h4>
+					<ul className="list-unstyled">
+						<li className="pb-1">
+							<Link to="/quienes-somos" className="link-light link-offset-2">agregar categorias xd</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="col-12 col-md-4 ">
+					<h4>Acerca de nosotros</h4>
+					<ul className="list-unstyled">
+						<li className="pb-1">
+							<Link to="/quienes-somos" className="link-light link-offset-2">Quiénes somos</Link>
+						</li>
+						<li className="pb-1">
+							<Link to="/contactanos" className="link-light link-offset-2">Contáctanos</Link>
+						</li>
+						<li className="pb-1">
+							<Link to="/preguntas-frecuentes" className="link-light link-offset-2">Preguntas frecuentes</Link>
+						</li>
+					</ul>
+				</div>
+				<div className="col-12 col-md-4">
+					<h4>Nuestras politicas</h4>
+					<ul className="list-unstyled">
+						<li className="pb-1">
+							<Link to="/politica-de-cancelacion" className="link-light link-offset-2">Política de cancelación</Link>
+						</li>
+						<li className="pb-1">
+							<Link to="/politica-de-reembolso" className="link-light link-offset-2">Política de reembolsos</Link>
+						</li>
+						<li className="pb-1">
+							<Link to="/politica-de-privacidad" className="link-light link-offset-2">Política de cancelación</Link>
+						</li>
+						<li className="pb-1">
+							<Link to="/terminos-y-co" className="link-light link-offset-2">Terminos y condiciones</Link>
+						</li>
+					</ul>
+				</div>
+
+				<div className="col-12 text-center py-3">
+					<p>
+						© 2025, nombre de la tienda-xd
+					</p>
+				</div>
+			</div>
+		</div>
+	</footer >
 );
