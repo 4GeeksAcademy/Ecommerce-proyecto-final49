ad229d391219ee1dd417ca82fe379322aae85bc9
JoseDHernandez <168580718+Josedavidh@users.noreply.github.com>
2025-06-22 22:21:23 +0000

    progress

--- src/front/pages/VistaProducto.jsx
+++ src/front/pages/VistaProducto.jsx
@@ -5,12 +5,20 @@ export const VistaProducto = () => {
     return (
         <div className="container">
             <div className="row">
+                <div className="col-2 d-flex flex-column gap-2">
+                    <img src="https://picsum.photos/id/101/60" className="img-thumbnail" alt="miniatura 1" />
+                    <img src="https://picsum.photos/id/102/60" className="img-thumbnail" alt="miniatura 2" />
+                    <img src="https://picsum.photos/id/103/60" className="img-thumbnail" alt="miniatura 3" />
+                    <img src="https://picsum.photos/id/104/60" className="img-thumbnail" alt="miniatura 4" />
+                </div>
                 <div className="col-6 col-md-4 py-4">
                     <img src="https://picsum.photos/id/2/300" alt="Imagen del producto" className="img-fluid" />
                 </div>
                 <div className="col-6 col-md-4 py-4">
                     <p><strong>Laptop VSAP VNJH1402 14.1'' Intel Celeron N4020 8gb Ram 256gb Ssd Windows 11 Pro Color Plateado</strong></p>
                     {/* <p>{estrellas} ({calificacion}/5)</p> */}
+                    <h3>$373<sup className="fs-6">45</sup></h3>
+                    <span>IVA incluido</span>
                 </div>
                 <div className="col-6 col-md-4 py-4">
 
