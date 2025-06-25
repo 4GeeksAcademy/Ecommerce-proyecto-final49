0d55618259945389604e18358b0cb45e155f1ba7
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-25 18:05:27 +0000

    la vista de recuperar contraseña fue añadida y espera a definir el estilo ademas se justifico el navbar a el final de la pagina en todas las vistas y se corrigio un error que hacia que el navbar sobresaliera en el eje x

--- src/front/pages/IniciarSesion.jsx
+++ src/front/pages/IniciarSesion.jsx
@@ -1,105 +1,105 @@
-import { useState } from "react"
-import useGlobalReducer from "../hooks/useGlobalReducer"
+import { useState } from "react";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
-import { Link, useNavigate, Navigate } from "react-router-dom"
+import { Link, useNavigate, Navigate } from "react-router-dom";
 
 const initialStateUser = {
   email: "",
-  password: ""
-}
+  password: "",
+};
 
 export const IniciarSesion = () => {
+  const [user, setUser] = useState(initialStateUser);
 
-  const [user, setUser] = useState(initialStateUser)
-
-  const { dispatch, store } = useGlobalReducer()
-  const navigate = useNavigate()
+  const { dispatch, store } = useGlobalReducer();
+  const navigate = useNavigate();
 
   const handleChange = ({ target }) => {
     setUser({
       ...user,
-      [target.name]: target.value
-    })
-  }
+      [target.name]: target.value,
+    });
+  };
 
   const handleSubmit = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const url = import.meta.env.VITE_BACKEND_URL
+    const url = import.meta.env.VITE_BACKEND_URL;
 
     const response = await fetch(`${url}/login`, {
       method: "POST",
       headers: {
-        "Content-type": "application/json"
+        "Content-type": "application/json",
       },
-      body: JSON.stringify(user)
-    })
+      body: JSON.stringify(user),
+    });
 
-    const data = await response.json()
+    const data = await response.json();
 
     if (response.ok) {
       localStorage.setItem("token", data.token);
       dispatch({ type: "LOGIN", payload: data.token });
       setTimeout(() => {
         navigate("/");
-      }, 2000)
+      }, 2000);
     } else if (response.status === 400) {
-      alert("El correo electrónico o la contraseña son incorrectos.")
+      alert("El correo electrónico o la contraseña son incorrectos.");
     } else {
-      alert("Error al iniciar sesión. Por favor, comunícate con soporte.")
+      alert("Error al iniciar sesión. Por favor, comunícate con soporte.");
     }
-  }
+  };
 
   if (store.token) {
-    return <Navigate to="/" />
+    return <Navigate to="/" />;
   }
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col-12 col-md-6 text-center justify-content-center p-5 h-100">
-          <h1>Ingresa tu correo electrónico y contraseña para iniciar sesión</h1>
+      <div className="row py-3">
+        <div className="col-12 col-md-6 d-flex align-items-center">
+          <h1 className="text-center">
+            Ingresa tu correo electrónico y contraseña para iniciar sesión
+          </h1>
         </div>
-        <div className="col-12 col-md-6" >
-          <form
-            onSubmit={handleSubmit}
-            className="border m-2 p-3">
-              <div className="form-group mb-3">
-                <label htmlFor="email">Correo electrónico</label>
-                <input
-                  type="email"
-                  placeholder="Ingresa tu correo electrónico"
-                  className="form-control border border-dark my-2"
-                  id="email"
-                  name="email"
-                  onChange={handleChange}
-                  value={user.email}
-                  required
-                />
-              </div>
-              <div className="form-group mb-3">
-                <label htmlFor="password" className="">Contraseña</label>
-                <input
-                  type="password"
-                  placeholder="Ingresa tu contraseña"
-                  className="form-control border-dark"
-                  id="password"
-                  name="password"
-                  onChange={handleChange}
-                  value={user.password}
-                  required
-                />
-              </div>
-              <button type="submit" className="btn btn-success w-100">
-                Iniciar sesión
-              </button>
+        <div className="col-12 col-md-6">
+          <form onSubmit={handleSubmit} className="border m-2 p-3">
+            <div className="form-group mb-3">
+              <label htmlFor="email">Correo electrónico:</label>
+              <input
+                type="email"
+                placeholder="Ingresa tu correo electrónico"
+                className="form-control border border-dark my-2"
+                id="email"
+                name="email"
+                onChange={handleChange}
+                value={user.email}
+                required
+              />
+            </div>
+            <div className="form-group mb-3">
+              <label htmlFor="password" className="">
+                Contraseña:
+              </label>
+              <input
+                type="password"
+                placeholder="Ingresa tu contraseña"
+                className="form-control border-dark"
+                id="password"
+                name="password"
+                onChange={handleChange}
+                value={user.password}
+                required
+              />
+              <Link to="/recuperar-contraseña"></Link>
+            </div>
+            <button type="submit" className="btn btn-success w-100">
+              Iniciar sesión
+            </button>
           </form>
         </div>
-        <h5 className="text-center mb-5">
+        <h5 className="text-center my-4">
           ¿No tienes cuenta? <Link to="/registro">Crea una</Link>
         </h5>
       </div>
     </div>
-
-
-  )
-}
+  );
+};
--- src/front/pages/Registro.jsx
+++ src/front/pages/Registro.jsx
@@ -1,119 +1,143 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const initialStateUser = {
-    lastname: "",
-    email: "",
-    password: "",
-}
+  lastname: "",
+  email: "",
+  password: "",
+  avatar: null,
+};
 
 export const Registro = () => {
-    const [user, setUser] = useState(initialStateUser)
+  const [user, setUser] = useState(initialStateUser);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
-    const handleChange = ({ target }) => {
-        setUser({
-            ...user,
-            [target.name]: target.value
-        })
+  const handleChange = ({ target }) => {
+    setUser({
+      ...user,
+      [target.name]: target.value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append("lastname", user.lastname);
+    formData.append("email", user.email);
+    formData.append("password", user.password);
+
+    if (user.avatar) {
+      formData.append("avatar", user.avatar);
     }
-    const handleSubmit = async (event) => {
-        event, preventDefault()
 
-        const formData = new formData()
-        formData.append("lastname", user.lastmane)
-        formData.append("email", user.email)
-        formData.append("password", user.password)
+    const url = import.meta.env.VITE_BACKEND_URL;
 
-        const url = import.meta.env.VITE_BACKEND_URL
+    try {
+      const response = await fetch(`${url}/register`, {
+        method: "POST",
+        body: formData,
+      });
 
-        const response = await fetch(`${url}/register`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: formData
-        })
-        if (response.status === 201) {
-            setUser(initialStateUser)
-            setTimeout(() => {
-                navigate("/login")
-            }, 2000)
-        } else if (respose.status === 400) {
-            alert("El usuario ya existe")
-        } else {
-            alert("Error al registrar usuario, intente nuevamente y si el problema persiste contactese con soporte")
-        }
+      if (response.status === 201) {
+        setUser(initialStateUser);
+        setTimeout(() => {
+          navigate("/login");
+        }, 2000);
+      } else if (response.status === 400) {
+        alert("El usuario ya existe");
+      } else {
+        alert(
+          "Error al registrar usuario, intente nuevamente y si el problema persiste contactese con soporte"
+        );
+      }
+    } catch (error) {
+      alert("Error de red o del servidor");
+      console.error(error);
     }
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-12 col-md-6 text-center my-5 justify-content-center p-5 h-100">
-                    <h1>Completa los datos para crear tu cuenta</h1>
-                </div>
-                <div className="col-12 col-md-6" >
-                    <form
-                        className="border m-2 p-3"
-                        onSubmit={handleSubmit}
-                    >
-                        <div className="form-group mb-3">
-                            <label htmlFor="btnName">Nombre Completo: </label>
-                            <input
-                                type="text"
-                                placeholder="Ingrese su nombre completo"
-                                className="form-control border-dark"
-                                id="btnName"
-                                name="lastname"
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="btnEmail">Correo electronico: </label>
-                            <input
-                                type="text"
-                                placeholder="Ingrese su correo electronico"
-                                className="form-control border-dark"
-                                id="btnEmail"
-                                name="email"
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="btnAvatar">Imagen de perfil: </label>
-                            <input
-                                type="file"
-                                className="form-control border-dark"
-                                id="btnAvatar"
-                                name="avatar"
-                                onChange={(event) => {
-                                    setUser({
-                                        ...user,
-                                        avatar: event.target.files[0]
-                                    })
-                                }}
-                            />
-                        </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="btnPass">Contraseña: </label>
-                            <input
-                                type="password"
-                                placeholder="ingrese una contraseña"
-                                className="form-control border-dark"
-                                id="btnPass"
-                                name="password"
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <button
-                            className="btn btn-success w-100"
-                        >Registrar</button>
-                    </form>
-                </div>
+  };
 
-                <h5 className="text-center my-4">
-                    ¿Ya tiene una cuenta? <Link to="/iniciar-sesion"> Inicia sesion</Link>
-                </h5>
-            </div>
+  return (
+    <div className="container-fluid d-flex">
+      <div className="row py-3">
+        <div
+          className="col-12 col-md-6 d-flex align-items-center justify-content-center">
+          <h1 className="text-center">
+            Completa los datos para crear tu cuenta
+          </h1>
         </div>
-    )
-}
+
+        <div className="col-12 col-md-6">
+          <form className="border m-2 p-3" onSubmit={handleSubmit}>
+            <div className="form-group mb-3">
+              <label htmlFor="btnName">Nombre Completo: </label>
+              <input
+                type="text"
+                placeholder="Ingrese su nombre completo"
+                className="form-control border-dark"
+                id="btnName"
+                name="lastname"
+                value={user.lastname}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="btnEmail">Correo electrónico: </label>
+              <input
+                type="email"
+                placeholder="Ingrese su correo electrónico"
+                className="form-control border-dark"
+                id="btnEmail"
+                name="email"
+                value={user.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="btnAvatar">Imagen de perfil: </label>
+              <input
+                type="file"
+                className="form-control border-dark"
+                id="btnAvatar"
+                name="avatar"
+                accept="image/*"
+                onChange={(event) => {
+                  setUser({
+                    ...user,
+                    avatar: event.target.files[0],
+                  });
+                }}
+              />
+            </div>
+
+            <div className="form-group mb-3">
+              <label htmlFor="btnPass">Contraseña: </label>
+              <input
+                type="password"
+                placeholder="Ingrese una contraseña"
+                className="form-control border-dark"
+                id="btnPass"
+                name="password"
+                value={user.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <button className="btn btn-success w-100" type="submit">
+              Registrar
+            </button>
+          </form>
+        </div>
+
+        <h5 className="text-center my-4">
+          ¿Ya tiene una cuenta? <Link to="/iniciar-sesion">Inicia sesión</Link>
+        </h5>
+      </div>
+    </div>
+  );
+};
