b3c5e3e2f5e3479a1b3cfe32bb0fc89c60984e74
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-25 22:25:00 +0000

    los cambios menores en las vistas genericas y el footer fueron realizadas

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export const Footer = () => (
   <div className="container-fluid">
-    <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 border-top bg-dark text-white px-5">
-      <div className="col mb-3">
+    <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-4 border-top bg-dark text-white px-5">
+      <div className="col mb-4">
         <Link
           to="/"
-          className="d-flex align-items-center mb-3 link-light text-decoration-none"
+          className="d-flex align-items-center mb-4 link-light text-decoration-none"
           aria-label="Inicio"
         >
           {/* Aquí puedes insertar tu logo SVG o un ícono */}
@@ -17,9 +17,9 @@ export const Footer = () => (
         <p className="text-light">© 2025 Nombre de la tienda</p>
       </div>
 
-      <div className="col mb-3"></div>
+      <div className="col mb-4"></div>
 
-      <div className="col mb-3">
+      <div className="col mb-4">
         <h5>Categorías</h5>
         <ul className="nav flex-column">
           <li className="nav-item mb-2">
@@ -30,7 +30,7 @@ export const Footer = () => (
         </ul>
       </div>
 
-      <div className="col mb-3">
+      <div className="col mb-4">
         <h5>Acerca de nosotros</h5>
         <ul className="nav flex-column">
           <li className="nav-item mb-2">
@@ -54,7 +54,7 @@ export const Footer = () => (
         </ul>
       </div>
 
-      <div className="col mb-3">
+      <div className="col mb-4">
         <h5>Nuestras políticas</h5>
         <ul className="nav flex-column">
           <li className="nav-item mb-2">
