ef5deeb9555aa63cb235a59109338f6af1df5613
Felipe Moreno <85708817+blacklottus-09@users.noreply.github.com>
2025-06-27 22:28:29 +0000

    el error que destruyo la galaxia... y el codigo fue solucionado

--- src/api/models.py
+++ src/api/models.py
@@ -4,38 +4,35 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 import datetime, validates
 
 db = SQLAlchemy()
+# necesita de conexion con la base de datos antes de ser implementado
+# class Role(db.Model):
+#     __tablename__ = 'role'
+#     id: Mapped[int] = mapped_column(primary_key=True)
+#     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+#     users: Mapped[list['User']] = relationship(back_populates='role')
 
-
-class Role(db.Model):
-    __tablename__ = 'role'
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    users: Mapped[list['User']] = relationship(back_populates='role')
-
-    def __repr__(self):
-        return f'<Role {self.name}>'
+#     def __repr__(self):
+#         return f'<Role {self.name}>'
 
 class User(db.Model):
     __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(
-        String(120), unique=True, nullable=False)
-    name: Mapped[str] = mapped_column(String(120), nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False) 
-    salt: Mapped[str] = mapped_column(String(80), nullable=False, default="") 
-    is_active: Mapped[bool] = mapped_column(
-        Boolean, nullable=False, default=True)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(120), nullable=False) 
+    password: Mapped[str] = mapped_column(String(255), nullable=False)   
+    salt: Mapped[str] = mapped_column(String(80), nullable=False, default="")   
+    is_active: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
+    # rompe el codigo si no existe la base de datos
+    # role_id: Mapped[int] = mapped_column(ForeignKey('role.id'), nullable=False)
     
-    role_id: Mapped[int] = mapped_column(ForeignKey('role.id'), nullable=False)
+    # role: Mapped['Role'] = relationship(back_populates='users')
     
-    role: Mapped['Role'] = relationship(back_populates='users')
-
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             "name": self.name,
-            "role": self.role.name if self.role else None 
+            "role": self.role.name if self.role else 2
         }
 
 
